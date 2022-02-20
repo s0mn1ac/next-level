ff59e7d44341d2e1e533ed2769c8a58ed732c8aa
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2022-02-20 11:25:19 +0000

    feat: añadida una nueva página: settings

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'settings',
+    loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
+  },
 ];
 
 @NgModule({

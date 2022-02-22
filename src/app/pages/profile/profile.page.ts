62861e735019a62527cb92e951998a8fc4417ad2
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2022-02-22 20:32:20 +0000

    wip: diseño de la pantalla profile

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfilePage implements OnInit {
 
+  public breakpoints: number[] = [0, 0.3];
+  public initialBreakpoint = 0.3;
+
   constructor() { }
 
   ngOnInit() {

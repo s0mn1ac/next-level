0a3ef7e59b83548bf19ee52eee8a36b98050ca5b
Juan Miguel González Machado <juanmi.gonzalezm@gmail.com>
2022-04-19 10:25:25 +0100

    feat: el cambio de color en la aplicación solo está disponible para usuarios pro

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { RoleEnum } from 'src/app/shared/enums/role.enum';
 import { UserStructure } from 'src/app/shared/interfaces/user-structure.interface';
 import { UserService } from 'src/app/shared/services/user.service';
 
@@ -11,12 +13,17 @@ export class SettingsPage implements OnInit {
 
   public languageSelected: string;
   public themeSelected: string;
+
+  public isProUser: boolean;
   public isDarkModeEnabled: boolean;
+
+  private user$: Subscription;
 
   constructor(private userService: UserService) { }
 
   ngOnInit() {
     this.setInitialData();
+    this.initSubscriptions();
   }
 
   public onChangeLanguage(event: any): void {
@@ -31,12 +38,21 @@ export class SettingsPage implements OnInit {
     this.userService.modifyUser('theme', event?.detail?.value);
   }
 
+  private initSubscriptions(): void {
+    this.user$ = this.userService.userObservable.subscribe((value: UserStructure) => this.setUserStructure(value));
+  }
+
   private setInitialData(): void {
     const userStructure: UserStructure = JSON.parse(localStorage.getItem('next-level-user'));
+    this.setUserStructure(userStructure);
+  }
+
+  private setUserStructure(userStructure: UserStructure): void {
     if (userStructure != null) {
       this.languageSelected = userStructure.language;
       this.isDarkModeEnabled = userStructure.mode === 'dark';
       this.themeSelected = userStructure.theme;
+      this.isProUser = userStructure.role === RoleEnum.pro || userStructure.role === RoleEnum.admin;
     }
   }
 
